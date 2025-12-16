e932a908e543d23b6458b20ac55c0f0232818a9b
Student <student@compass.local>
2025-12-16 12:21:46 +0600

    commit message

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-const button = document.getElementById('button')
+const { createElement } = require("react");
+
+const button = document.getElementById('button');
 
 
 const customAlert = (message) => {
-    alert(message)
+    const overlay = document.createElement('div');
+    overlay.className = 'overlay';
+    document.body.append(overlay);
+
+    const contentDiv = document.createElement('div');
+    contentDiv.className = 'content';
+    overlay.append(contentDiv);
+
+    const messageH1 = document.createElement('h1');
+    messageH1.textContent = message;
+    contentDiv.append(messageH1);
+
+    const closeButton = document.createElement('button');
+    closeButton.className = ' closeBtn';
+    closeButton.textContent ='Ok';
+    contentDiv.append(closeButton);
+
+    closeButton.addEventListener('click', () => {
+        overlay.remove();
+    })
+
+    overlay.addEventListener('click', (event) => {
+        if(event.target.className === 'overlay') {
+            overlay.remove();
+        }
+    })
+    
+
+
+
+
 }
 button.addEventListener('click', () => {
-    customAlert('Learn frontend!')
+    customAlert('Learn frontend')
 })
